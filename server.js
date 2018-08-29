23a1ab85028c4a8055653e3decf7291853358dd2
gavin-love <1co.loco@oprotonmail.ch>
2018-08-29 15:11:05 -0600

    Add GET for locations and breweries

--- server.js
+++ server.js
@@ -1,17 +1,41 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.set('port', process.env.PORT || 3000);
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 3001);
 
 app.get('/api/v1/locations', (request, response) => {
-  return response.status(200).json('a list of states');
+  database('locations').select()
+    .then(locations => {
+      return response.status(200).json(locations);
+    })
+    .catch(err => {
+      return res.status(500).json({ err })
+    })
 })
+
+app.get('/api/v1/breweries', (request, response) => {
+
+  database('breweries').select()
+    .then(breweries => {
+      return response.status(200).json(breweries);
+    })
+    .catch(err => {
+      return res.status(500).json({ err })
+    })
+});
 
 app.get('api/v1/locations/:state');
 
 app.get('api/v1/locations/:city');
 
-app.get('api/v1/breweries');
 
 app.get('api/v1/breweries/:type');
 
@@ -26,3 +50,7 @@ app.delete('api/v1/breweries');
 app.put('api/v1/breweries/:name');
 
 app.put('api/v1/locations/:city');
+
+app.listen(app.get('port'), () => {
+  console.log(`you are listening on port ${app.get('port')}`)
+})
